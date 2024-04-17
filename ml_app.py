9f5f00ce2f7353f7026f843326fb05ecdb637075
1NT20CV002 <167279314+1NT20CV002@users.noreply.github.com>
2024-04-17 12:33:48 +0530

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -9,7 +9,7 @@ from sklearn.tree import DecisionTreeClassifier
 
 model = DecisionTreeClassifier(criterion='entropy', max_depth=10, min_samples_leaf=5,random_state=0)
 
-model = joblib.load(finalized_model.joblib)
+model = joblib.load('finalized_model.joblib')
 
 @st.cache
 

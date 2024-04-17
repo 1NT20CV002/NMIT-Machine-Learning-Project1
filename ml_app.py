31e34759fa630d5acf254fbd715b72774e41e7d8
1NT20CV002 <167279314+1NT20CV002@users.noreply.github.com>
2024-04-17 12:37:48 +0530

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -20,7 +20,7 @@ def predict (buying,maint,doors,persons,lug_boot,safty):
     safty=2
   elif safty=='low':
     safty=3
-  df = pd.Dataframe([Buying, Maint, Doors, Persons, Lug_boot, safty],columns=(['buying','maint','doors','persons','lug_boot','safty'])
+  df = pd.Dataframe([Buying, Maint, Doors, Persons, Lug_boot, safty],columns=['buying','maint','doors','persons','lug_boot','safty'])
   prediction =model.predict([[Buying, Maint, Doors, Persons, Lug_boot, safty]])
   return prediction
 
